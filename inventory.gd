7a6e2523ffe172067ad9727f4bf70b3e222b9f6a
KubaPlay <jakub.muzik2007@gmail.com>
2025-12-08 11:31:04 +0100

    Inventář, fix labelu

--- inventory.gd
+++ inventory.gd
@@ -1,19 +1,20 @@
 extends CanvasLayer
 
+var inventory = [null, null, null, null]
+var current_scene_path: String
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	visible = false # Po startu menu schováme
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
+func _ready() -> void:
+	visible = false
+	current_scene_path = get_tree().current_scene.get_path()
+	print("Aktuální cesta scény: ", current_scene_path)
 func _input(event):
 	if event.is_action_pressed("open_inv"): 
-		if PauseMenu.visible:
+		if PauseMenu.visible or current_scene_path == "/root/StartMenu":
 			print("nejde...")
+			return
 		else:
 			toggle_inv()
 
--- pause_menu.gd
+++ pause_menu.gd
@@ -1,15 +1,19 @@
 extends CanvasLayer
 
 
+var current_scene: String
 
 func _on_ready() -> void:
 	visible = false # Po startu menu schováme
+	current_scene = get_tree().current_scene.get_path()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"): # Obvykle ESC
-		if Inventory.visible:
+		if Inventory.visible and current_scene != "/root/StartMenu":
 			Inventory.visible = false
 			toggle_pause()
+		elif current_scene == "/root/StartMenu":
+			get_tree().quit()
 		else:
 			toggle_pause()
 
