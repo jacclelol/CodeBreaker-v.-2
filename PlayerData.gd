883667a2549b4ad531a0d3b67ec84d33a2a58f2a
jacclelol <muzikj.07@spst.eu>
2026-03-20 08:40:59 +0100

    Finalizace

--- PlayerData.gd
+++ PlayerData.gd
@@ -4,4 +4,3 @@ var clothes = "pyjamas"
 var maobleceni = false
 var ma_klic: bool = false
 var inventar: Array = []
-# var velikost_postavy: Vector2 = ... (pokud by sis chtěl pamatovat i to zmenšování)
